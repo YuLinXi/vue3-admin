e73c9c6cd4e414236563ac5431917030b65b700d
YuLinXi <yumengyuan@klicen.com>
2021-01-11 18:07:07 +0800

    :tada: 项目初始化

---
+++ src/views/login/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">
+    123
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  components: {
+  },
+})
+export default class Login extends Vue {
+}
+</script>
